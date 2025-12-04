2768be136fb17aa6e20eabed388384a0917c83d3
aleks <aleksiya.solovyova@student.kdg.be>
2025-12-05 00:22:13 +0100

    Evaluating baseline

--- src/train_baseline.py
+++ src/train_baseline.py
@@ -1,15 +1,21 @@
 from stable_baselines3 import PPO, DQN, A2C, SAC
 import gymnasium as gym
+
+from src.blackjack_wrapper import BlackjackWrapper
 from utils import load_config
 from stable_baselines3.common.callbacks import CheckpointCallback
 import os
 
 
-def train_baseline(cfg_path="config/config_baseline.yaml"):
+def train_baseline(cfg_path="../config/config_baseline.yaml"):
     cfg = load_config(cfg_path)
-    for trial in cfg['num_trials']:
-        env =  # Create environment
-        algo =  # Pick Algorithm
+    for trial in range(1, cfg['num_trials'] + 1):
+        env =  BlackjackWrapper(gym.make(cfg['environment'], render_mode=None))
+        algo =  globals()[cfg['algorithm']]
+
+        os.makedirs(f"results/baseline/{cfg['algorithm']}_trial{trial}", exist_ok=True)
+        os.makedirs(f"logs/baseline/{cfg['algorithm']}_trial{trial}", exist_ok=True)
+
         model = algo("MlpPolicy", env, verbose=1, tensorboard_log=f"logs/baseline/{cfg['algorithm']}_trial{trial}/")
         cb = CheckpointCallback(save_freq=cfg["checkpoint_freq"],
                                 save_path=f"logs/baseline/{cfg['algorithm']}_trial{trial}/checkpoints/")
